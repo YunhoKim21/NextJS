faf1315f5da1362fef7ab142b3338508a3b3bbfc
name <F35jsflove@gmail.com>
2022-08-02 21:54:31 -0700

    bugfix

--- pages/react-projects/counter-2.jsx
+++ pages/react-projects/counter-2.jsx
@@ -1,5 +1,5 @@
 import { Button } from '@nextui-org/react'
-import Connect4 from '@/components/Connect4'
+import Connect4 from '@/components/Counter'
 
 export default function Game() {
   return <Connect4 />
