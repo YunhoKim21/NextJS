9af6507a8d8904fa027866ee8771f6c184fb0e13
name <F35jsflove@gmail.com>
2022-08-27 16:16:58 +0900

    convex hull

--- utils/math.js
+++ utils/math.js
@@ -22,4 +22,8 @@ function twoLineIntersection(x1, y1, x2, y2, x3, y3, x4, y4) {
   return [x, y]
 }
 
-export { rInt, in_range, twoLineIntersection }
+function ccw(x1, y1, x2, y2, x3, y3) {
+  return (x1 - x2) * (y3 - y2) - (y1 - y2) * (x3 - x2)
+}
+
+export { rInt, in_range, twoLineIntersection, ccw }
